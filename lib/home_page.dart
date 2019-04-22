5fff58bbd5836d59182874356a12e6d7ad480a84
Hiroki Furuichi <hirokicodes@gmail.com>
2019-04-23 00:15:12 +0900

    fetches data from url and displays data in list

---
+++ lib/home_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  final List currencies;
+  HomePage(this.currencies);
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List currencies = [];
+  final List<MaterialColor> _colors = [Colors.blue, Colors.indigo, Colors.red];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Crypto Info'),
+        ),
+        body: _cryptoWidget());
+  }
+
+  Widget _cryptoWidget() {
+    return Container(
+        child: Column(children: <Widget>[
+      Flexible(
+          child: ListView.builder(
+              itemCount: widget.currencies.length,
+              itemBuilder: (BuildContext context, int index) {
+                final Map currency = widget.currencies[index];
+                final MaterialColor color = _colors[index % _colors.length];
+
+                return _getListItemUI(currency, color);
+              }))
+    ]));
+  }
+
+  ListTile _getListItemUI(Map currency, MaterialColor color) {
+    return ListTile(
+      leading:
+          CircleAvatar(backgroundColor: color, child: Text(currency['name'])),
+      title:
+          Text(currency['name'], style: TextStyle(fontWeight: FontWeight.bold)),
+      subtitle: _getSubtitleText(
+          currency['price_usd'], currency['percent_change_1h']),
+      isThreeLine: true,
+    );
+  }
+}
+
+Widget _getSubtitleText(String priceUSD, String percentageChange) {
+  TextSpan priceTextWidget =
+      TextSpan(text: '\$$priceUSD\n', style: TextStyle(color: Colors.black));
+  String percentageChangeText = '1 hour $percentageChange%';
+  TextSpan percentageChangeTextWidget;
+
+  if (double.parse(percentageChange) > 0) {
+    percentageChangeTextWidget = TextSpan(
+        text: percentageChangeText, style: TextStyle(color: Colors.green));
+  } else {
+    percentageChangeTextWidget = TextSpan(
+        text: percentageChangeText, style: TextStyle(color: Colors.red));
+  }
+
+  return RichText(
+      text: TextSpan(children: [priceTextWidget, percentageChangeTextWidget]));
+}
